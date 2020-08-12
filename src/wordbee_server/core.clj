561f96957ac53102b0015f5a36fade5aeac5f314
Pushpankar <pushpankark@gmail.com>
2020-08-12 17:25:24 +0530

    Respond on success from ignore-word

--- src/wordbee_server/core.clj
+++ src/wordbee_server/core.clj
@@ -18,7 +18,6 @@
   (let [word (get-in request [:params "word"])
         level2-words (filterv #(= (get-in @data/data [:difficulty (keyword %)]) 2) (:all-words @data/data))
         word-index (.indexOf level2-words word)]
-    (println word-index)
     (response {:word (get level2-words (+ 1 word-index))
                :sorrounding (subvec level2-words (- word-index 5) (+ word-index 5))})))
 
@@ -28,30 +27,32 @@
 (defn get-module [request]
   (let [id (get-in request [:params :id])]
     ;; @TODO return data with words => IN V2
-    (response {:word-list (get (:module data/data) id)})))
+    (response {:word-list (get (:module @data/data) id)})))
 
 ;; Create a module and update words definitions
 (defn add-module [request]
   (let [update-words (fn [module]
                        ;; Take every word map, find it location and update it
-                       (map #(update-in data/data [:database (:word %)] %) module))
+                       (map #(update-in @data/data [:database (:word %)] %) module))
         new-module (:body request)
         new-words (map :word new-module)]
-    (reset! data/data (update data/data :module conj new-words)) ;; Need to ensure that a word had not been sent for editing twice
+    (reset! data/data (update @data/data :module conj new-words)) ;; Need to ensure that a word had not been sent for editing twice
     (reset! data/data (update-words new-module))
-    (reset! data/data (update data/data :tracked-words into new-words))
+    (reset! data/data (update @data/data :tracked-words into new-words))
     (response {:result "OK"})))
 
 ;; This function is required since I can't know from add-module fn
 ;; which word had been ignored
 (defn ignore-word [request]
-  (let [word (get-in request [:params :word])]
-    (reset! data/data (update data/data :ignored-words conj word))))
+  (let [word (get-in request [:params "word"])]
+    (reset! data/data (update @data/data :ignored-words conj word))
+    (response {:result "OK"})))
 
 (defroutes routes
   (POST "/get-module" [] get-module)
   (POST "/add-module" [] add-module)
-  (POST "/next-word" [] next-word))
+  (POST "/next-word" [] next-word)
+  (POST "/ignore-word" [] ignore-word))
 
 (def app
   (-> routes
