fa4931dee70a93708e8fa537e5e782bb0937de32
System administrator <root@ip-172-31-38-239.ap-south-1.compute.internal>
2020-11-06 04:18:20 +0000

    New deployment server config

--- server.nix
+++ server.nix
@@ -1,6 +1,24 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, modulesPath, ... }: {
+  imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
+  ec2.hvm = true;
+  
+  environment.systemPackages = 
+    [ pkgs.vim
+      pkgs.htop
+      pkgs.tmux
+      pkgs.git
+      pkgs.leiningen
+      pkgs.openjdk11
+      pkgs.mongodb 
+      pkgs.mongodb-tools 
+    ];
 
-{
+  services = {
+    mongodb = {
+      enable = true;
+    };
+  };
+
   security.acme.acceptTerms = true;
   security.acme.email = "pushpankark@gmail.com";
   services.nginx = {
@@ -11,7 +29,15 @@
     virtualHosts."launchitbeforeyoumakeit.com" = {
       enableACME = true;
       forceSSL = true;
-      locations."/".root = "${pkgs.nginx}/html";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+      };
     };
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [80 443 3000 8443];
   };
 }
