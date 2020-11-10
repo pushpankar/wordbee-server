86a10b9c7a9675454c5fe7bf4cc08a1cb33bc20b
Pushpankar <pushpankark@gmail.com>
2020-11-10 18:23:13 +0530

    Setup development server with repl support

--- src/wordbee_server/core.clj
+++ src/wordbee_server/core.clj
@@ -71,11 +71,10 @@
   (route/expand-routes
    #{["/greet" :get respond-hello :route-name :greet]}))
 
-(defn create-server []
-  (http/create-server
+(def service-map
    {::http/routes routes
     ::http/type   :jetty
-    ::http/port   3000}))
+    ::http/port   3000})
 
 ;; (defroutes routes
 ;;   (POST "/get-module" [] get-module)
@@ -93,7 +92,20 @@
 
 (defn start! []
   ;; (db/init-db!)
-  (http/start (create-server)))
+  (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server (http/start (http/create-server
+                              (assoc service-map ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
 
 (defn -main
   "Entry point"
