05bb4821f90b3e7a3176074ec28cfe665380d126
Pushpankar <pushpankark@gmail.com>
2020-11-12 00:32:42 +0530

    Remove old codes

--- src/wordbee_server/core.clj
+++ src/wordbee_server/core.clj
@@ -65,31 +65,12 @@
                   response (ok context)]
               (assoc context :response response)))})
 
-;; utils
-;; (defn parse-module [module]
-;;   (try (-> module (subs 1) Integer/parseInt (* 1000))
-;;        (catch Exception ex "all")))
-
-
-;; ;; This function is not at all used
-;; ;; I am infering last used word from tracked list
-;; (defn ignore-word [request]
-;;   (let [word (get-in request [:body "word"])]
-;;     ;; (reset! data/data (update @data/data :ignored-words conj word))
-;;     (response {:result "OK"})))
-
-
 (defn surrounding-words [word]
   (let [word-index (.indexOf db/ordered-words word)
         start (max 0 (- word-index 5))
         end (min (+ word-index 6) (count db/ordered-words))]
     (subvec db/ordered-words start end)))
 
-;; get word from the database and attach surrounding words
-;; (defn get-word [request]
-;;   (let [word ]
-;;     (println (:request request))
-;;     (ok (assoc (db/get-word word) :surrounding-words (surrounding-words word)))))
 (def next-word
   {:name :next-word
    :enter (fn [context]
@@ -109,37 +90,12 @@
             (let [word (get-in context [:request :path-params :word])]
               (assoc context :result (db/get-word word))))})
 
-
-;; ;; This is a wrapper for get-word
-;; (defn next-word-api [request]
-;;   (let [module (parse-module (get-in request [:body "path"]))
-;;         word (or (get-in request [:body "word"]) (db/last-word module))
-;;         word-defn (db/next-word word)]
-;;     (response (assoc word-defn :surrounding-words (surrounding-words (:word word-defn))))))
-
-
-;; ;; The client should ask for a module id
-;; ;; @TODO Need to be updated
-;; (defn get-module [request]
-;;   (let [id (get-in request [:body "id"])
-;;         module-words (db/module-words id)]
-;;     {:status 200
-;;      :body (map db/get-word module-words)}))
-
 (def get-module
   {:name :get-module
    :enter (fn [context]
             (let [module-id (get-in context [:request :path-params :id])
                   module-words (db/module-words module-id)]
               (assoc context :result (map db/get-word module-words))))})
-
-;; (defn save-word [request]
-;;   (let [module (parse-module (get-in request [:body "path"]))
-;;         word-data (:body request)
-;;         word (get word-data "word")]
-;;     (db/update-word (walk/keywordize-keys word-data))
-;;     (db/add-to-module word module)
-;;     (response {:result "OK"})))
 
 (def update-word
   {:name :update-word
@@ -149,10 +105,6 @@
               (db/add-to-module (:word data) "all")
               (assoc context :result (created (:word data)))))})
 
-;; (defn list-modules [_]
-;;   (let [module-names (db/module-names)]
-;;     (response {:result "OK"
-;;                :data (map (fn [key] {:key key :words (db/module-words key)}) module-names)})))
 
 (def list-modules
   {:name :list-modules
@@ -178,14 +130,6 @@
    {::http/routes routes
     ::http/type   :jetty
     ::http/port   3000})
-
-;; (defroutes routes
-;;   (POST "/get-module" [] get-module)
-;;   (POST "/save-word" [] save-word)
-;;   (POST "/get-word" [] get-word)
-;;   (POST "/next-word" [] next-word-api)
-;;   (POST "/list-modules" [] list-modules)
-;;   (POST "/modules-info" [] modules-info)
 
 ;;   ;; Game routes
 ;;   (GET "/new-game" req (game/new-game req))
