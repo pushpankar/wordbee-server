8a01483895a53186831296efa265a71637721a28
Pushpankar <pushpankark@gmail.com>
2020-11-13 01:08:44 +0530

    Add doc and reformat

--- src/wordbee_server/ws.clj
+++ src/wordbee_server/ws.clj
@@ -8,37 +8,44 @@
 
 
 (def ws-clients (atom {}))
-(def pairs      (atom {}))
+(def pairs (atom {}))
 
 (def random-pool (async/chan 10))
 
 ;; utils
-(defn send-msg [userid msg]
+(defn send-msg
+  "Wrapper function for finding client and sending message"
+  [userid msg]
   (async/put! (get @ws-clients userid) (pr-str msg)))
 
 ;; Pairs users for the game
 (go-loop []
-         (let [user1 (<! random-pool)
-               user2 (<! random-pool)]
-           (swap! pairs assoc user1 user2 user2 user1)
-           (send-msg user1 [:paired {:opponent user2}])
-           (send-msg user2 [:paired {:opponent user1}])))
+  (let [user1 (<! random-pool)
+        user2 (<! random-pool)]
+    (swap! pairs assoc user1 user2 user2 user1)
+    (send-msg user1 [:paired {:opponent user2}])
+    (send-msg user2 [:paired {:opponent user1}])))
 
 ;; Connection clean
-(defn close-later [session-id send-ch]
+(defn close-later
+  "A cleanup function without this global pairs will keep
+  increasing in size. There will be nothing to worry about
+  even if user disconnects without notification."
+  [session-id send-ch]
   (go
     (<! (async/timeout 300000))
     (swap! ws-clients dissoc session-id)
     (dissoc pairs session-id)))
 
 (defn new-ws-client
+  "New session handler"
   [ws-session send-ch]
   (let [sessionid (nano-id)]
-    (async/put! send-ch  (pr-str [:connected sessionid]))
+    (async/put! send-ch (pr-str [:connected sessionid]))
     (swap! ws-clients assoc sessionid send-ch)
     (close-later sessionid send-ch)))
 
-(defmulti msg-handler #(first %))
+(defmulti msg-handler first)
 
 (defmethod msg-handler :default [params]
   (println params))
@@ -55,16 +62,16 @@
 (defn close-handler [code msg]
   (match [(edn/read-string msg)]
          [{:userid userid}] (swap! ws-clients dissoc userid)
-         :else              nil)
+         :else nil)
   (log/info :msg "WS closed: " :reason msg)
   )
 
 (def ws-paths
   {"/ws" {:on-connect (ws/start-ws-connection new-ws-client)
-          :on-text   #(-> % edn/read-string msg-handler)
-          :on-binary (fn [payload offset length] (log/info :msg "Binary Message!" :bytes payload))
-          :on-error (fn [t] (log/error :msg "WS Error happened" :exception t))
-          :on-close close-handler}})
+          :on-text    #(-> % edn/read-string msg-handler)
+          :on-binary  (fn [payload offset length] (log/info :msg "Binary Message!" :bytes payload))
+          :on-error   (fn [t] (log/error :msg "WS Error happened" :exception t))
+          :on-close   close-handler}})
 
 
 (defn context-configurator-fn [inp]
