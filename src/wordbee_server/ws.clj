3ce426c552f07e8ef31df3d83b4ded2603ef83aa
Pushpankar <pushpankark@gmail.com>
2020-11-12 21:44:26 +0530

    Implement websocket

--- src/wordbee_server/ws.clj
+++ src/wordbee_server/ws.clj
@@ -2,35 +2,37 @@
   (:require [clojure.core.async :as async :refer [go <! >!]]
             [io.pedestal.log :as log]
             [io.pedestal.http.jetty.websockets :as ws]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [nano-id.core :refer [nano-id]]))
 
 
 (def ws-clients (atom {}))
 
 (defn new-ws-client
   [ws-session send-ch]
-  (println ws-session send-ch)
-  (async/put! send-ch "Hello from server. You are connected")
-  ;; You are connect, Here is your session id
-  ;; User sends pair me
-  ;; On pair send user you have been paired
-  (swap! ws-clients assoc ws-session send-ch))
+  (let [sessionid (nano-id)]
+    (async/put! send-ch  (pr-str [:connected sessionid]))
+    (swap! ws-clients assoc sessionid send-ch)))
 
 
-(defmulti msg-handler #(-> % edn/read-string first))
+(defmulti msg-handler #(first %))
+
+(defmethod msg-handler :default [params]
+  (println params))
 
 (defmethod msg-handler :hello [params]
   (println "hello")
   (println params))
 
-(defmethod msg-handler :ping-pong [_ data]
+(defmethod msg-handler :ping-pong [[_ userid msg]]
   (async/go
+    (println "Ping-pong")
     (<! (async/timeout 3000))
-    (println "After a second")))
+    (async/put! (get @ws-clients userid) (pr-str [:ping-pong {:msg "Stop ping pong"}]))))
 
 (def ws-paths
   {"/ws" {:on-connect (ws/start-ws-connection new-ws-client)
-          :on-text   msg-handler
+          :on-text   #(-> % edn/read-string msg-handler)
           :on-binary (fn [payload offset length] (log/info :msg "Binary Message!" :bytes payload))
           :on-error (fn [t] (log/error :msg "WS Error happened" :exception t))
           :on-close (fn [num-code reason-text]
